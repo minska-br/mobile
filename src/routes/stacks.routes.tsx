4173c3eb9790b9314f4cc3eb4c02b89a0eebc1b2
Barreto <v.barreto.and@gmail.com>
2021-09-02 08:23:29 -0300

    feat: add new routes for result list and result detail screens

--- src/routes/stacks.routes.tsx
+++ src/routes/stacks.routes.tsx
@@ -3,14 +3,21 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import Home from "../pages/Home";
 import Search from "../pages/Search";
 import RoutesEnum from "../enums/routes";
+import ResultList from "../pages/ResultList";
+import ResultDetail from "../pages/ResultDetail";
 
 const Stack = createNativeStackNavigator();
 
 const AppRoutes: React.FC = () => {
   return (
-    <Stack.Navigator screenOptions={{ headerShown: false }}>
+    <Stack.Navigator
+      initialRouteName={RoutesEnum.Home}
+      screenOptions={{ headerShown: false }}
+    >
       <Stack.Screen name={RoutesEnum.Home} component={Home} />
       <Stack.Screen name={RoutesEnum.Search} component={Search} />
+      <Stack.Screen name={RoutesEnum.ResultList} component={ResultList} />
+      <Stack.Screen name={RoutesEnum.ResultDetail} component={ResultDetail} />
     </Stack.Navigator>
   );
 };

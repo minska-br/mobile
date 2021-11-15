2553826d93707e4069dfb71f67448d150313a349
Leonardo Morais Leme <leonardo3morais7@gmail.com>
2021-11-15 11:35:36 -0300

    chore(SearchResult): trying to call web crawled endpoint directly

--- src/pages/SearchResult.tsx
+++ src/pages/SearchResult.tsx
@@ -8,17 +8,18 @@ import RoutesEnum from "../enums/routes";
 import { SessionContext } from "../contexts/SessionContext";
 import Container from "../components/Container";
 import MinskaApi from "../services/MinskaApi";
-import { AxiosResponse } from "axios";
+import axios, { AxiosResponse } from "axios";
 import notify from "../helpers/notify";
 
 export default function SearchResult({ route, navigation }: any) {
   const [data, setData] = useState<any>([]);
-  const { setLoadingStatus } = useContext(SessionContext);
-  const { seachItem, activeFluxType } = route.params;
+  const { activeFluxType, setLoadingStatus } = useContext(SessionContext);
+  const { seachItem } = route.params;
 
   const renderItem = ({ item }: any) => {
     const onPressItem = () => {
       setLoadingStatus(true);
+      console.log("[SearchResult] renderItem(item)", item);
       navigation.navigate(RoutesEnum.CalculatingEmptyState);
     };
 
@@ -30,14 +31,21 @@ export default function SearchResult({ route, navigation }: any) {
   };
 
   const getList = async () => {
-    let response!: AxiosResponse<any>;
+    let response!: any;
+    // let response!: AxiosResponse<any> | undefined;
     try {
-      response = await MinskaApi.getProductList(seachItem.name);
-      const { data } = response;
-      setData(data);
+      // response = await MinskaApi.getProductList(seachItem.name.trim());
+      const url = `http://127.0.0.1:4390/recipes/allrecipes?value=${seachItem.name.trim()}`;
+      console.log("[SearchResult] getList", { url });
+
+      response = await MinskaApi.getProductList(seachItem.name.trim());
+
+      console.log("[SearchResult] getList(response): ", response);
+      // const data = response?.data;
+      // setData(data);
       setLoadingStatus(false);
     } catch (error) {
-      console.error("[SearchResult] getList: ", { response });
+      console.error("[SearchResult] getList: ", error);
       notify("Erro inesperado, tente novamente mais tarde.");
       setLoadingStatus(false);
     }

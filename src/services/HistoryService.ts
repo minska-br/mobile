bdf491f41a3810d31dc5f46d54dd21c1a3da66d8
Leonardo Morais <leonardo3morais7@gmail.com>
2021-11-15 22:44:40 -0300

    feat(HistoryService): add save scheduling items function

--- src/services/HistoryService.ts
+++ src/services/HistoryService.ts
@@ -1,11 +1,19 @@
 const INIT = "@Minska:";
 import AsyncStorage from "@react-native-async-storage/async-storage";
+import HistoryItem from "../interfaces/HistoryItem";
 
 const logging = (method: string, value?: any) =>
   console.log(`\n[HistoryService/${method}]`, { value });
 
 export default class HistoryService {
   private static getKey = (key: string) => INIT + key;
+
+  static saveScheduling = async (item: HistoryItem) => {
+    const key = this.getKey(item.id);
+    const value = JSON.stringify(item);
+    logging("saveSchedule", { key, value });
+    await AsyncStorage.setItem(key, value);
+  };
 
   static getAllKeys = async () => {
     logging("getAllKeys");

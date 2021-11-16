9554971c6c1dc779b972b8c5c9ca1e428e9c18f9
Thales Pereira <thalespereiradejesus@gmail.com>
2021-11-15 21:09:21 -0300

    feat(MinskaService): fix logs and add feature to get calculation

--- src/services/MinskaService.ts
+++ src/services/MinskaService.ts
@@ -1,7 +1,15 @@
 import MinskaApi from "../apis/MinskaApi";
+import HistoryItem from "../interfaces/HistoryItem";
 
 class MinskaService {
+  /**
+   * Return a recipe or a product usign name as parameter to search.
+   *
+   * @param name Recipe or product name
+   * @returns
+   */
   static getExistingItemByName = async (name: string) => {
+    console.log("\n[MinskaService] getExistingItemByName: ", { name });
     const requests = await MinskaApi.getAllRequests();
     const data = requests.data;
 
@@ -18,6 +26,18 @@ class MinskaService {
     return existingItemWithTheSameName;
   };
 
+  static getCalculation = async (calculationId: string) => {
+    console.log("\n[MinskaApi] getCalculation", { calculationId });
+    try {
+      const response = await MinskaApi.findCalculation(calculationId);
+      const data = response.data;
+      return data;
+    } catch (error) {
+      console.error("\n[MinskaService] getCalculation | ERROR: " + JSON.stringify(error));
+      return null;
+    }
+  };
+
   /**
    * This method returns the calculationId of the scheduled item.
    *
@@ -31,7 +51,9 @@ class MinskaService {
       const data = response.data;
       return data.calculationId;
     } catch (error) {
-      console.error("\n[MinskaService] getExistingItemByName | ERROR: " + JSON.stringify(error));
+      console.error(
+        "\n[MinskaService] scheduleProductCalculation | ERROR: " + JSON.stringify(error)
+      );
     }
   };
 

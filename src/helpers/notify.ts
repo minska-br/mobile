3a9fbea23039990548dcd01fdb56330e13c98ddb
Barreto <v.barreto.and@gmail.com>
2021-11-17 18:07:21 -0300

    feat(notify): reause the native feature to show toast messages

--- src/helpers/notify.ts
+++ src/helpers/notify.ts
@@ -1,14 +1,13 @@
-// import Toast from "react-native-root-toast";
+import Toast from "react-native-root-toast";
 
 /**
  * @param message Text to show in the toast.
  * @param shortDuration By default 'false', switch duration from LONG to SHORT.
  */
 const notify = (message: string, shortDuration: boolean = false) => {
-  // const { SHORT, LONG } = Toast.durations;
-  // const duration = shortDuration ? SHORT : LONG;
-  // Toast.show(message, { duration });
-  alert(message);
+  const { SHORT, LONG } = Toast.durations;
+  const duration = shortDuration ? SHORT : LONG;
+  Toast.show(message, { duration });
 };
 
 export default notify;
